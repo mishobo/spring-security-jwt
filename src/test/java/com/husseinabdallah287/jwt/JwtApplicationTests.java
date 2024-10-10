2c2aab21059681adb49c91d2cb1654d0ad325e98
mishobo <husseinabdalla287@gmail.com>
2024-10-10 23:23:01 +0300

    first commit

---
+++ src/test/java/com/husseinabdallah287/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.husseinabdallah287.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
